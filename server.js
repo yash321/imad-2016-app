2dda310ab157909e81a76ab352cc3672295f347f
yash321 <mittal.yash254@gmail.com>
2016-09-23 17:43:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article one',(req,res))
+{
+    res.send('article one is requested and will be served');
+}
+app.get('/article two',(req,res))
+{
+    res.send('article two is requested and will be served');
+}
+app.get('/article three',(req,res))
+{
+    res.send('article three is requested and will be served');
+}
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
