580dc3c0784000daede68cc00e46270c11cdbbc3
yash321 <mittal.yash254@gmail.com>
2016-09-23 18:04:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article one',function(req,res)
+app.get('/articleone',function(req,res)
 {
     res.send('article one is requested and will be served');
 });
-app.get('/article two',function(req,res)
+app.get('/articletwo',function(req,res)
 {
     res.send('article two is requested and will be served');
 });
-app.get('/article three',function(req,res)
+app.get('/articlethree',function(req,res)
 {
     res.send('article three is requested and will be served');
 });
