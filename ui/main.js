4a3694b84bcd98e360cdb835c05faf1dd94c16c9
yash321 <mittal.yash254@gmail.com>
2016-09-29 22:31:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,18 @@ img.onclick=function(){
 var button= document.getElementById('counter');
 button.onclick=function()
 {
-  counter=counter+1;
-  var span =document.getElementById('count');
-  span.innerHTML=counter.toString();
+  var request= new XMLHttpRequest();
+  request.onreadystatechange=function()
+  {
+      if(request.readystate===XMLHttpRequest.DONE)
+      {
+          if(request.status===200)
+          {
+              var counter= request.ResponsiveText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
+      }
+  };
+  
 };
