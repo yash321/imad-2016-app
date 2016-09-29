52a9fb2b47219dd0c72536926f8a93d03ba4c0c4
yash321 <mittal.yash254@gmail.com>
2016-09-29 14:49:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('maintext');
 element.innerHTML='NEW VALUE';
+var im= document.getElementById('bg');
+bg.onclick=function(){
+    bg.style.marginleft='100px';
+}
