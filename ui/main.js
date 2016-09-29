89c4523048ba7f4a377fc835d48dd4aab66c051c
yash321 <mittal.yash254@gmail.com>
2016-09-29 15:07:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,10 @@ element.innerHTML='NEW VALUE';
 var img= document.getElementById('bg');
 var marginLeft=0;
 function moveright(){
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+5;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
-    var interval=setInterval(moveright,100);
+    var interval=setInterval(moveright,50);
     
 }
