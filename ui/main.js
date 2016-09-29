8adac85cadfbb629c59e08b0cb963b89f6663182
yash321 <mittal.yash254@gmail.com>
2016-09-29 16:30:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
 var img= document.getElementById('bg');
 var marginLeft=0;
+var counter=0;
 function moveright(){
     marginLeft=marginLeft+5;
     img.style.marginLeft=marginLeft+'px';
@@ -8,4 +9,11 @@ function moveright(){
 img.onclick=function(){
     var interval=setInterval(moveright,50);
     
-}
+};
+var button= document.getElementById('counter');
+button.onclick=function()
+{
+  counter=counter+1;
+  var span =document.getElementById('count');
+  span.innerHTML=count.toString();
+};
