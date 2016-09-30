64f71bb9e007f4728a6c0b51c16915d484f1cead
yash321 <mittal.yash254@gmail.com>
2016-09-30 19:07:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,5 +26,21 @@ button.onclick=function()
           }
       }
   };
+  request.open('GET','http://yash321.imad.hasura-app.io/counter');
+  request.send(null);
   
+};
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('sub');
+submit.onclick=function()
+{
+  var names=['name 1','name 2','name 3'];
+  var list='';
+  for(var i=0;i<names.length;i++)
+  {
+      list+='<li>'+names[i]+'<li>';
+  }
+  var ul=document.getElementById('namelist');
+  ul.innerHTML=list;
 };
